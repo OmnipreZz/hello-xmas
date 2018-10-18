29b61c1cc4cbf5b01a061edfd9ae7f44597a35c9
Geoffrey <geoffrey.frioli@gmail.com>
2018-10-18 21:17:13 +0200

    step 2

---
+++ public/app.es6.js
@@ -0,0 +1,25 @@
+let array = [];
+
+const hello = () => {
+
+    //Input name
+    let input = document.getElementById('name').value;
+    name = input !== "" ? input : "Père Noël";
+    let maj = name.split('');
+    maj[0] = maj[0].toUpperCase();
+    let ok = maj.join('');
+    array.push(ok)
+
+    //message
+    let message = document.getElementById('message');
+    message.innerHTML = `Bonjour ${name} !`;
+
+    //List
+    var li = document.createElement("li");
+    let list = document.getElementById('list');
+    li.innerHTML = ok;
+    document.getElementById("list").append(li);
+}
+
+
+document.getElementById('button').onclick = hello;

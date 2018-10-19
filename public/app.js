0375716b7293fc8860fc17e00ea93b71b0ed7275
Geoffrey <geoffrey.frioli@gmail.com>
2018-10-19 11:59:47 +0200

    compile with babel

--- public/app.js
+++ public/app.js
@@ -1,54 +1,54 @@
-let array = [];
+"use strict";
 
-const hello = () => {
+var array = [];
+
+var hello = function hello() {
 
     //Input name
-    let input = document.getElementById('name').value;
+    var input = document.getElementById('name').value;
     name = input !== "" ? input : "Père Noël";
-    let maj = name.split('');
+    var maj = name.split('');
     maj[0] = maj[0].toUpperCase();
-    let ok = maj.join('');
-    array.push(ok)
+    var ok = maj.join('');
+    array.push(ok);
 
     //message
-    let message = document.getElementById('message');
-    message.innerHTML = `Bonjour ${ok} !`;
+    var message = document.getElementById('message');
+    message.innerHTML = "Bonjour " + ok + " !";
 
     //List
-    let li = document.createElement("li");
+    var li = document.createElement("li");
     li.innerHTML = ok;
     document.getElementById("list").append(li);
+};
 
-}
-
-const search = () => {
+var search = function search() {
 
     //Compare
-    let nameSearch = document.getElementById('search').value;
-    let maj = nameSearch.split('');
+    var nameSearch = document.getElementById('search').value;
+    var maj = nameSearch.split('');
     maj[0] = maj[0].toUpperCase();
-    let ok = maj.join('');
-    let compare = array.filter(name => name.startsWith(ok));
+    var ok = maj.join('');
+    var compare = array.filter(function (name) {
+        return name.startsWith(ok);
+    });
 
     //Affiche
-    let affiche = document.getElementById('nameSearch');
-    affiche.innerHTML = compare;  
-}
+    var affiche = document.getElementById('nameSearch');
+    affiche.innerHTML = compare;
+};
 
 document.getElementById('button').onclick = hello;
 document.getElementById('searchButton').onclick = search;
 
 //Vider l'input text au click
-let vide = document.getElementById('button');
-vide.addEventListener('click', () => {
+var vide = document.getElementById('button');
+vide.addEventListener('click', function () {
     document.getElementById("name").value = "";
 });
 
 //Vider l'input search au click
-let vide1 = document.getElementById('searchButton');
-vide1.addEventListener('click', () => {
+var vide1 = document.getElementById('searchButton');
+vide1.addEventListener('click', function () {
     document.getElementById("search").value = "";
 });
-
-
-
--- src/app.es6.js
+++ src/app.es6.js
@@ -14,13 +14,38 @@ const hello = () => {
     let message = document.getElementById('message');
     message.innerHTML = `Bonjour ${ok} !`;
 
-    
     //List
     let li = document.createElement("li");
-    let list = document.getElementById('list');
     li.innerHTML = ok;
     document.getElementById("list").append(li);
 
 }
 
+const search = () => {
+
+    //Compare
+    let nameSearch = document.getElementById('search').value;
+    let maj = nameSearch.split('');
+    maj[0] = maj[0].toUpperCase();
+    let ok = maj.join('');
+    let compare = array.filter(name => name.startsWith(ok));
+
+    //Affiche
+    let affiche = document.getElementById('nameSearch');
+    affiche.innerHTML = compare;
+}
+
 document.getElementById('button').onclick = hello;
+document.getElementById('searchButton').onclick = search;
+
+//Vider l'input text au click
+let vide = document.getElementById('button');
+vide.addEventListener('click', () => {
+    document.getElementById("name").value = "";
+});
+
+//Vider l'input search au click
+let vide1 = document.getElementById('searchButton');
+vide1.addEventListener('click', () => {
+    document.getElementById("search").value = "";
+});

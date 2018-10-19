09f046464a8a8052c98a9d8a5facf1d21bb5b91b
Geoffrey <geoffrey.frioli@gmail.com>
2018-10-19 13:36:50 +0200

    rdy for deployement

---
+++ public/app.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var array = [];
+
+var hello = function hello() {
+
+    //Input name
+    var input = document.getElementById('name').value;
+    name = input !== "" ? input : "Père Noël";
+    name = name.toLowerCase();
+    var maj = name.split('');
+    maj[0] = maj[0].toUpperCase();
+    var ok = maj.join('');
+    array.push(ok);
+
+    //message
+    var message = document.getElementById('message');
+    message.innerHTML = "Bonjour " + ok + " !";
+
+    //List
+    var li = document.createElement("li");
+    li.innerHTML = ok;
+    document.getElementById("list").append(li);
+};
+
+var search = function search() {
+
+    //Compare
+    var nameSearch = document.getElementById('search').value;
+    nameSearch = nameSearch.toLowerCase();
+    var maj = nameSearch.split('');
+    maj[0] = maj[0].toUpperCase();
+    var ok = maj.join('');
+    var compare = array.filter(function (name) {
+        return name.startsWith(ok);
+    });
+
+    //Affiche
+    var affiche = document.getElementById('nameSearch');
+    affiche.innerHTML = compare;
+};
+
+document.getElementById('button').onclick = hello;
+document.getElementById('searchButton').onclick = search;
+
+//Vider l'input text au click
+var vide = document.getElementById('button');
+vide.addEventListener('click', function () {
+    document.getElementById("name").value = "";
+});
+
+//Vider l'input search au click
+var vide1 = document.getElementById('searchButton');
+vide1.addEventListener('click', function () {
+    document.getElementById("search").value = "";
+});
